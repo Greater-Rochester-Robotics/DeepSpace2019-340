6c7a36c102a86772d91c404a415186fc417278ad
Devinaut <jdaleygaming@gmail.com>
2019-03-21 19:13:27 -0400

    Switched drive motors to Spark MAX via PWM

--- DeepSpace-2019/src/main/java/frc/robot/subsystems/Drive.java
+++ DeepSpace-2019/src/main/java/frc/robot/subsystems/Drive.java
@@ -13,8 +13,8 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.PWMSparkMax;
 import frc.robot.RobotMap;
 import frc.robot.commands.DriveXOne;
 
@@ -31,8 +31,8 @@ public class Drive extends Subsystem {
 	//RIP IMU
 	// private static ADIS16448_IMU imu;
 	private static ADXRS450_Gyro gyro;
-	private static Encoder encLeft, encRight; //TODO: adjust for circumference
-	private static Spark driveLeft, driveRight;
+	private static Encoder encLeft, encRight;
+	private static PWMSparkMax driveLeft, driveRight;
 	private static TalonSRX mantisLeft, mantisRight;
 
 	/**
@@ -57,8 +57,8 @@ public class Drive extends Subsystem {
 		encLeft.setReverseDirection(true);
 		encRight.setReverseDirection(true);
 
-		driveLeft = new Spark(RobotMap.DRIVE_SPARK_LEFT_CHANNEL);
-		driveRight = new Spark(RobotMap.DRIVE_SPARK_RIGHT_CHANNEL);
+		driveLeft = new PWMSparkMax(RobotMap.DRIVE_SPARK_LEFT_CHANNEL);
+		driveRight = new PWMSparkMax(RobotMap.DRIVE_SPARK_RIGHT_CHANNEL);
 
 		driveLeft.setInverted(true); //Negate all speeds to the left side to account for mirrored axes
 
